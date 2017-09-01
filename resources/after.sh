793a43c68f25b2d0e1e84832f4a8c4774f2e0526
Nick Barrett <nick@juicebox.com.au>
2017-09-01 10:42:30 +0800

    added a composer installer

--- resources/after.sh
+++ resources/after.sh
@@ -3,3 +3,9 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
+
+sudo a2enmod ssl
+sudo apt-get -y install libapache2-mod-fastcgi php-apcu
+sudo a2enmod actions fastcgi vhost_alias
+sudo chown vagrant: /var/lib/apache2/fastcgi
+sudo service apache2 restart
